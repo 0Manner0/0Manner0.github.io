2f26e555fcd329b7f461e7420b3e7dd08f593427
wangjunfei <1151995480@qq.com>
2020-08-04 21:04:32 +0800

    fix.

--- test/main.js
+++ test/main.js
@@ -57,13 +57,13 @@ window.boot = function () {
     function setLoadingDisplay () {
         // Loading splash scene
         var splash = document.getElementById('splash');
-        // var progressBar = splash.querySelector('.progress-bar span');
-        // cc.loader.onProgress = function (completedCount, totalCount, item) {
-        //     var percent = 100 * completedCount / totalCount;
-        //     if (progressBar) {
-        //         progressBar.style.width = percent.toFixed(2) + '%';
-        //     }
-        // };
+        //var progressBar = splash.querySelector('.progress-bar span');
+        //cc.loader.onProgress = function (completedCount, totalCount, item) {
+            //var percent = 100 * completedCount / totalCount;
+            //if (progressBar) {
+                //progressBar.style.width = percent.toFixed(2) + '%';
+            //}
+        //};
         splash.style.display = 'block';
         //progressBar.style.width = '0%';
 
